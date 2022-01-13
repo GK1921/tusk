c00fac308262bc54a1889eb7fbe51913e9f380da
Geethanjali Gopalkrishna <87863949+GK1921@users.noreply.github.com>
2022-01-13 11:03:34 +0530

    Update linearsearch.java

--- linearsearch.java
+++ linearsearch.java
@@ -24,3 +24,58 @@ public class Main{
 
 
 }
+
+//leetcode
+
+// https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
+
+import static java.lang.System.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {12,345,2,6,7896};
+        out.println(findNumbers(nums));
+
+    }
+    static int findNumbers(int[] nums){
+        int count = 0;
+        for(int num : nums){
+            if(even(num)){
+                count++;
+            }
+        }
+        return count;
+
+
+    }
+    static boolean even(int num) {
+        int numberOfDigits= digits(num);
+        if (numberOfDigits % 2 == 0){
+            return true;
+        }
+        return false;
+    }
+
+    static int digits(int num) {
+        if(num<0){
+            num = num * 1;
+        }
+        if(num==0){
+            return 1;
+
+        }
+
+        int count =0 ;
+        while (num > 0) {
+            count++;
+            num = num/10;
+
+        }
+        return count;
+    }
+
+
+
+
+    }
+
